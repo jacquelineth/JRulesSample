0e7cc5fb3158c0e4ba26fe608a6e9298cfbfd607
Thierry JACQUELINE <jacquelineth@gmail.com>
2014-03-25 11:56:06 +0100

    Added various constructor

--- ClassicXOM/src/com/ibm/support/PMR/SellableArticle.java
+++ ClassicXOM/src/com/ibm/support/PMR/SellableArticle.java
@@ -1,8 +1,25 @@
 package com.ibm.support.PMR;
 
+import java.lang.reflect.Field;
+
 public class SellableArticle implements Article {
 	public SellableArticle(String s) {
 		name=s;
+	}
+	public SellableArticle(String s, int p) {
+		this(s);
+		price=p;
+	}
+	public SellableArticle(String s, float p) {
+		this(s);
+		price=(int)p;
+	}
+	public SellableArticle(Object o) {
+		Class<? extends Object> c=o.getClass();
+		for (Field f :c.getDeclaredFields()){
+		System.err.println(	f.getType().toString());
+		}
+		System.err.println(o.toString());
 	}
 	private String name; 
 	public String getName() {
@@ -12,13 +29,13 @@ public class SellableArticle implements Article {
 	private int price;
 	@Override
 	public int getPrice() {
-		// TODO Auto-generated method stub
+
 		return price;
 	}
 
 	@Override
 	public Article getAffordableArticle(int max) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
