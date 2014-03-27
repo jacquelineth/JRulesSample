fcd98142e291812e2da6e30919ad49076effe08b
Thierry JACQUELINE <jacquelineth@gmail.com>
2014-03-27 12:47:39 +0100

    Added all SellableArticle constructors to match BOM usage

--- ClassicXOM/src/com/ibm/support/PMR/SellableArticle.java
+++ ClassicXOM/src/com/ibm/support/PMR/SellableArticle.java
@@ -4,11 +4,46 @@ public class SellableArticle implements Article {
 	public SellableArticle(String s) {
 		name=s;
 	}
+	public SellableArticle (String s, int i) {
+		this(s);
+		price= i;
+	}
+	public SellableArticle(String s, float f){
+		this(s,(int)f);
+	}
+	public SellableArticle(Object o) {
+		java.lang.reflect.Field[] Fs= o.getClass().getDeclaredFields();
+		for (java.lang.reflect.Field f: Fs){
+			try{
+			retrieveFieldMatching("[Pp]rice",o, f,this.getClass().getDeclaredField("price"));
+			retrieveFieldMatching("[Nn]ame",o, f,this.getClass().getDeclaredField("name"));
+			}
+			catch(Exception e){e.printStackTrace();}
+		}
+	}
+	/**
+	 * @param o
+	 * @param f
+	 */
+	private void retrieveFieldMatching(String matchingRegex,Object o, java.lang.reflect.Field source,java.lang.reflect.Field target) {
+		if(source.getName().matches(matchingRegex)){
+			try {
+				source.set(target, source.get(o));
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+	}
 	private String name; 
 	public String getName() {
 		return name;
 	}
-	
+	int test$ee;
 	private int price;
 	@Override
 	public int getPrice() {
